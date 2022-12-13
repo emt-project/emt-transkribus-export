8bb36227498e0ab4f67a3b6749390049ec1fc5f1
csae8092 <peter.andorfer@oeaw.ac.at>
2022-12-13 15:56:25 +0100

    added script to create entity tables

--- scripts/make_entity_tables.py
+++ scripts/make_entity_tables.py
@@ -1,30 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[12]:
-
-
 import pandas as pd
 
-
-# In[14]:
-
-
-df = pd.read_csv('../metadata.csv')
-
-
-# In[15]:
-
-
-wer_an_wen = set(df['weranwen'].values)
-
-
-# In[16]:
+df = pd.read_csv("../metadata.csv")
+wer_an_wen = set(df["weranwen"].values)
 
 
 data = []
 persons = set()
-split_token = ' an '
+split_token = " an "
 for x in wer_an_wen:
     sender = ""
     receiver = ""
@@ -33,75 +15,22 @@ for x in wer_an_wen:
             sender, receiver = x.split(split_token)
     except TypeError:
         continue
-    if sender.endswith(','):
+    if sender.endswith(","):
         sender = sender[:-1]
     persons.add(sender.strip())
     persons.add(receiver.strip())
 
-# In[17]:
-
-
 data = []
 for i, x in enumerate(sorted(persons)):
-    item = {
-        "name": x,
-        "gnd": "",
-        "emt_id": f"emt_person_{i+1}"
-    }
+    item = {"name": x, "gnd": "", "emt_id": f"emt_person_{i+1}"}
     data.append(item)
-
-
-# In[18]:
-
 
 person_df = pd.DataFrame(data)
-
-
-# In[20]:
-
-
-person_df.to_csv('../persons.csv', index=False)
-
-
-# In[26]:
-
-
-orte = [x for x in set(df['Ort'].values) if isinstance(x, str)]
-
-
-# In[30]:
-
-
+person_df.to_csv("../persons.csv", index=False)
+orte = [x for x in set(df["Ort"].values) if isinstance(x, str)]
 data = []
 for i, x in enumerate(sorted(orte)):
-    item = {
-        "name": x,
-        "geonames": "",
-        "emt_id": f"emt_place_{i+1}"
-    }
+    item = {"name": x, "geonames": "", "emt_id": f"emt_place_{i+1}"}
     data.append(item)
-
-
-# In[31]:
-
-
 places = pd.DataFrame(data)
-
-
-# In[34]:
-
-
-places.to_csv('../places.csv', index=False)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+places.to_csv("../places.csv", index=False)
