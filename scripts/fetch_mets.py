5073c1bbec03efdd9cedd583c5b62ff32ce5d416
csae8092 <peter.andorfer@oeaw.ac.at>
2022-12-15 08:51:14 +0100

    download mets files for several collections

--- scripts/fetch_mets.py
+++ scripts/fetch_mets.py
@@ -1,9 +1,10 @@
 import os
+import pandas as pd
+from tqdm import tqdm
 from transkribus_utils.transkribus_utils import ACDHTranskribusUtils
 
 user = os.environ.get('TR_USER')
 pw = os.environ.get('TR_PW')
-col_id = os.environ.get('COL_ID')
 
 transkribus_client = ACDHTranskribusUtils(
     user=user,
@@ -11,4 +12,7 @@ transkribus_client = ACDHTranskribusUtils(
     transkribus_base_url="https://transkribus.eu/TrpServer/rest"
 )
 
-mpr_docs = transkribus_client.collection_to_mets(col_id, file_path='./mets')
+df = pd.read_csv("../col_ids.csv")
+for i, row in tqdm(df.iterrows(), total=len(df)):
+    col_id = row["coll_id"]
+    mpr_docs = transkribus_client.collection_to_mets(col_id, file_path='./mets')
