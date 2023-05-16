f2beed190deba56f1422bc8ced87eb278be6ca48
csae8092 <peter.andorfer@oeaw.ac.at>
2023-05-16 14:22:55 +0200

    wip

--- scripts/fetch_single_mets.py
+++ scripts/fetch_single_mets.py
@@ -8,9 +8,12 @@ pw = os.environ.get("TR_PW")
 col_id = os.environ.get("colId")
 doc_id = os.environ.get("docId")
 
+
+mets_dir = os.path.join(".", "mets", col_id)
+print(mets_dir)
 transkribus_client = ACDHTranskribusUtils(
     user=user, password=pw, transkribus_base_url="https://transkribus.eu/TrpServer/rest"
 )
-mets_file = transkribus_client.save_mets_to_file(doc_id, col_id)
+mets_file = transkribus_client.save_mets_to_file(doc_id, col_id, file_path=mets_dir)
 
 print(mets_file)
